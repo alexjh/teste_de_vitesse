d80172fa94e2237d9b682330597c2b529cd83cff
Alex Harford <harford@gmail.com>
2014-06-04 21:54:59 -0700

    Invert test / loop to reduce # of tests

--- test/test.js
+++ test/test.js
@@ -1,55 +1,58 @@
-var assert = require("assert")
+var assert = require("assert");
 
 describe('Quiz', function(){
+
+    this.timeout(500);
+
     var quiz = require('../lib/questions');
     var min_target_number = 5;
     var max_target_number = 99;
     var pages = 1;
 
-    describe('Length', function(){
+    describe('Length (add)', function(){
         var quiz_questions = quiz.gen_question_array( 10, pages, true);
         it('should return an array with a multiple of pages * 20', function(){
             assert.equal( 20*pages, quiz_questions.length );
-        })
-    })
+        });
+    });
 
     // Check if answers are correct
     describe('Correct answers', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should return correct answers', function(){
+        it('should return correct answers', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, true);
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
                     assert.equal(   quiz_questions[j][0][0]
                                  + quiz_questions[j][0][1],
                                  quiz_questions[j][1] );
                 }
-            })
-        }
-    })
+            }
+        });
+    });
 
     // One or two questions that have 0 as an operand
     describe('adding zeroes', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should have one or two questions with 0 as an operand', function(){
+        it('should have one or two questions with 0 as an operand', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, true);
                 var num_zero = 0;
 
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
-                    if ( quiz_questions[j][0][0] == 0 || quiz_questions[j][0][1] == 0 ) {
+                    if ( quiz_questions[j][0][0] === 0 || quiz_questions[j][0][1] === 0 ) {
                         num_zero++;
                     }
                 }
 
                 assert.ok( num_zero == 1 || num_zero == 2 );
-            })
-        }
-    })
+            }
+        });
+    });
 
-    // 7 - 10 questions that are equal to the target number
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should have one or two questions with 0 as an operand', function(){
+    // 6 - 10 questions that are equal to the target number
+    describe('adding to target', function(){
+        it('should have 6 - 10 questions equal to the target number', function(){
+            for (var i = 9; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, true);
                 var num_target = 0;
 
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
@@ -58,71 +61,69 @@ describe('Quiz', function(){
                     }
                 }
 
-                assert.ok( num_target > 6 && num_target < 11 );
-            })
-        }
-    })
+                assert.ok( num_target > 5 && num_target < 11, "Target: " + num_target );
+            }
+        });
+    });
 
     // No answers greater than the target
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should never have an answer greater than the target', function(){
+    describe('overflow', function(){
+        it('should never have an answer greater than the target', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, true);
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
-                    assert.ok( quiz_questions[j][1] >= i );
+                    assert.ok( quiz_questions[j][1] <= i );
                 }
-            })
-        }
-    })
-
-
+            }
+        });
+    });
 
     // Subtraction:
     // Quiz length
-    describe('Length', function(){
-        var quiz_questions = quiz.gen_question_array( 10, pages, false);
+    describe('Length (sub)', function(){
+        var quiz_questions = quiz.gen_question_array( 20, pages, false);
         it('should return an array with a multiple of pages * 20', function(){
             assert.equal( 20*pages, quiz_questions.length );
-        })
-    })
+        });
+    });
 
     // Check if answers are correct
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should return correct answers', function(){
+    describe('correct answers (sub)', function(){
+        it('should return correct answers', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, false);
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
                     assert.equal(   quiz_questions[j][0][0]
-                                  - quiz_questions[j][0][1],
+                                 - quiz_questions[j][0][1],
                                  quiz_questions[j][1] )
                 }
-            })
-        }
-    })
+            }
+        });
+    });
 
     // Ensure that there are 2 - 4 questions that have 0 for the answer
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should have 2 - 4 questions that = 0', function(){
+    describe('subtraction to zero', function(){
+        it('should have 2 - 4 questions that = 0', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, false);
                 var num_zero = 0;
 
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
-                    if ( quiz_questions[j][1] == 0 ) {
+                    if ( quiz_questions[j][1] === 0 ) {
                         num_zero++;
                     }
                 }
 
                 assert.ok( num_zero > 1 && num_zero < 5 );
-            })
-        }
-    })
+            }
+        });
+    });
 
     // 6 - 8 questions that have the target as the first operand
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should have 6 - 8 questions with the target number as the first operand', function(){
+    describe('subtraction from target number', function(){
+        it('should have 6 - 8 questions with the target number as the first operand', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, false);
                 var num_target = 0;
 
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
@@ -132,22 +133,21 @@ describe('Quiz', function(){
                 }
 
                 assert.ok( num_target > 5 && num_target < 9 );
-            })
-        }
-    })
+            }
+        });
+    });
 
     // Ensure all answers are >= 0
-    describe('', function(){
-        for (var i = min_target_number; i <= max_target_number; i++) {
-            var quiz_questions = quiz.gen_question_array( i, pages, true);
-            it('should have answers >= 0', function(){
+    describe('underflow', function(){
+        it('should have answers >= 0', function(){
+            for (var i = min_target_number; i <= max_target_number; i++) {
+                var quiz_questions = quiz.gen_question_array( i, pages, false);
                 for ( var j = 0; j < quiz_questions.length; j++ ) {
                     assert.ok( quiz_questions[j][1] >= 0 );
                 }
 
-            })
-        }
-    })
-
-})
+            }
+        });
+    });
+});
 
